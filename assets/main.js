2247af039de5e76c28584db0dc22dac968a2e3f4
yknk <yknk@users.noreply.github.com>
2018-06-30 15:17:30 +0900

    assets/main.jsを更新

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,15 @@
   $(function() {
     $('a[href^="https://"], a[href^="http://"]').not('[href^="https://' + location.hostname + '"], [href^="http://' + location.hostname + '"]').not('[target]').attr('target', '_blank');
   });
+
+  if (!navigator.doNotTrack) {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-110781470-1', 'yknk.jp');
+    ga('set', 'anonymizeIp', true);
+    ga('send', 'pageview');
+  }
 })(jQuery);
