dff2a2261e7a531d3617e002f8c46cbbb918537e
yknk <yknk@users.noreply.github.com>
2018-07-12 23:52:29 +0900

    assets/main.jsを更新

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,8 @@
 (function($) {
   $(function() {
     $('a[href^="https://"], a[href^="http://"]').not('[href^="https://' + location.hostname + '"], [href^="http://' + location.hostname + '"]').not('[target]').attr('target', '_blank');
+
+    $('table').wrap('<div class="table-wrap"></div>');
   });
 
   if (!navigator.doNotTrack) {
